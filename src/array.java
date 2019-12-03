01d1c04703102bbb61eff497b893ad936988b11c
Ryan Moore <ryan.moore.2022@student.lhschools.org>
2019-12-03 09:53:06 -0500

    pattern 4

--- src/array.java
+++ src/array.java
@@ -1,3 +1,46 @@
 public class array {
-    
+    public static void main(String[] args){
+    Pattern1();
+    Pattern3();
+    Pattern4();
+    Pattern5();
+    }
+    public static void Pattern1(){
+        int maxTimes = 5;
+        for (int times = 6; times > 0; times--){
+            System.out.println("******");
+        }
+
+    }
+
+    public static void Pattern4(){
+        int maxTimes = 5;
+        for (int times = 6; times > 0; times--){
+            System.out.println("");
+            for (int j=times; j<7;j++) {
+                System.out.print("*");
+            }
+        }
+
+    }
+
+    public static void Pattern3(){
+        int maxTimes = 5;
+        for (int times = 6; times > 0; times--) {
+            System.out.println("");
+            for(int j=1; j<7; j++){
+                System.out.print(j);
+            }
+        }
+    }
+    public static void Pattern5(){
+        int maxTimes = 5;
+        int number = 1;
+        for (int times = 6; times > 0; times--){
+            System.out.println("");
+            for (int j=times; j<7;j++) {
+                System.out.print(number);
+            }
+        }
+    }
 }
